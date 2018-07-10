d94cbec04d5f0d8cf6798df37dc2b2af0ab83c60
sayersb <bradleysayers1@gmail.com>
2018-07-10 11:39:58 +0100

    notfoundjs page added

---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1 className="title">404: This event is no more sadly</h1>
+      <h2 className="subtitle">For all other events please use the links above</h2>
+    </div>
+  );
+};
+
+export default NotFound;
