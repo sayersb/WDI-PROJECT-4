f821a6f16b02db7bc46130fe1efb3926158e5ee8
sayersb <bradleysayers1@gmail.com>
2018-07-12 14:41:20 +0100

    seeds added

--- db/seeds.js
+++ db/seeds.js
@@ -31,6 +31,18 @@ mongoose.connect(dbURI, (err, db) => {
       image: 'https://rb-website.cdn.prismic.io/rb-website/a337ea29ee9e837e6401a2d002e20a5978ca3eaa_react-poster.jpg',
       description: 'React is having a huge impact on the way we think about Web UI development. These monthly Meetups are an opportunity to learn why and share experiences. This is a sociable group and very welcoming to newcomers',
       location: { lat: 51.5169128, lng: -0.1364268 }
+    }, {
+      name: 'Coding for women',
+      date: '2018-08-07',
+      image: 'https://www.popwebdesign.net/popart_blog/wp-content/uploads/2015/11/women-and-coding3.jpg',
+      description: 'Come and join us for our regular Tuesday evening meetup and participate in any one of a number of peer-led JavaScript coding activities, including freecodecamp and codewars challenges. This meetup is for women who want to learn to code. During the meetup we encourage participants to work together with another meetup attendee on one laptop (pair programming). Students from the current cohort of Founders & Coders will be available for questions and support.',
+      location: { lat: 51.5635499, lng: -0.1078269 }
+    }, {
+      name: 'Arsenal v Manchester City',
+      date: '2018-08-12',
+      image: 'https://as01.epimg.net/en/imagenes/2017/03/31/football/1490959169_888552_1490959224_noticia_normal.jpg',
+      description: 'Reigning Premier League champions Manchester City kick off the new season at the Emirates in North London for what will surely be an action packed match. Will be odd for the Arsenal fans not having Arsene Wenger in the dugout after 20+ years at the helm.',
+      location: { lat: 51.5548885, lng: -0.108438 }
     }]))
     .then(events => console.log(`${events.length} events created`))
     .catch(err => console.log(err))
