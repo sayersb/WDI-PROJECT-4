1b78bd679c418748161fe24760c36480e5ccf7b4
Antoni Alston <sntonialston@gmail.com>
2018-07-12 15:13:23 +0100

    started working on search function that returns events from ticketmaster

---
+++ src/components/common/Search.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import Suggestions from '../../components/common/Suggestions';
+import _ from 'lodash';
+
+// const { API_KEY } = 'lRu2r80C3Z2JXXYHXYiFiuAd5YClwEpN';
+// const API_URL = 'https://app.ticketmaster.com/discovery/v2/events';
+
+class Search extends Component {
+  state = {
+    query: '',
+    results: []
+  }
+
+  search = query => {
+    axios.get('https://app.ticketmaster.com/discovery/v2/events.json', {
+      params: {
+        keyword: query,
+        apikey: 'lRu2r80C3Z2JXXYHXYiFiuAd5YClwEpN'
+      }
+    })
+      .then(res => {
+        if(res.data._embedded) this.setState({ results: res.data._embedded.events });
+      });
+  }
+
+  debouncedSearch = _.debounce(this.search, 500);
+
+  handleChange = ({ target: { value }}) => {
+    if(value) this.debouncedSearch(value);
+  }
+
+  render() {
+    return (
+      <form>
+        <input
+          placeholder="Search for..."
+          onChange={this.handleChange}
+        />
+        <Suggestions results={this.state.results} />
+      </form>
+    );
+  }
+}
+
+export default Search;
