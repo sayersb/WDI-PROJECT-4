e6f41a6b4b732d1307bf8c8301556b03d9fa62a4
sayersb <bradleysayers1@gmail.com>
2018-07-17 16:28:12 +0100

    seeds added

--- src/components/events/Show.js
+++ src/components/events/Show.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import axios from 'axios';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import Auth from '../../lib/Auth';
 import Slider from 'react-slick';
 import Promise from 'bluebird';
@@ -10,18 +10,22 @@ import PayPalButton from '../common/PayPalButton';
 // import Sticky from 'react-sticky-state';
 // import { Player } from 'video-react';
 
-
-
 class EventsShow extends React.Component {
 
   constructor() {
     super();
     this.state ={
-
+      // event: {},
+      // events: []
     };
   }
 
   componentDidMount() {
+    Promise.props({
+      event: axios.get(`/api/events/${this.props.match.params.id}`).then(res => res.data),
+      events: axios.get('/api/events').then(res => res.data.filter((event, i) => i % 3 === 0))
+    })
+      .then(data => this.setState(data));
     axios.get(`/api/events/${this.props.match.params.id}`)
       .then( res => this.setState({ event: res.data }))
       .catch(err => this.setState({ error: err.message }));
@@ -43,12 +47,9 @@ class EventsShow extends React.Component {
     if(this.state.error) return <h2 className="title is-2">{this.state.error}</h2>;
     if(!this.state.event) return <h2 className="title is-2">Loading...</h2>;
     const settings = {
-      dots: true,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 1,
-      slidesToScroll: 1
-
+      centerMode: true,
+      centerPadding: '60px',
+      slidesToShow: 1
     };
     return (
       //     <Link className="button" to={`/events/${this.state.event._id}/edit`}>Edit</Link>
@@ -59,9 +60,7 @@ class EventsShow extends React.Component {
         <div className="container">
           <div className="columns">
             <div className="column is-four-fifths">
-              <figure className="images">
-                <img src={this.state.event.image} />
-              </figure>
+              <AudioImage className="audio-image" imageSrc={this.state.event.image} audioSrc={'/assets/applause.mp3'}/>
             </div>
             <div className="column">
               <h2 className="title">{this.state.event.name}</h2>
@@ -73,11 +72,8 @@ class EventsShow extends React.Component {
               <h3 className="subtitle"> Ticket Status : {this.state.event.status}</h3>
 
               <h3 className="subtitle"> Price  : £{this.state.event.price.toFixed(2)}</h3>
-
             </div>
           </div>
-          <div className="heading">A</div>
-
 
           <div className="heading">
 
@@ -85,9 +81,9 @@ class EventsShow extends React.Component {
               <form action="https://github.us18.list-manage.com/subscribe/post?u=59db66124013cd520fd3937ea&amp;id=7312abcb99" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate" target="_blank" noValidate>
                 <div id="mc_embed_signup_scroll">
                   {/* <label htmlFor="mce-EMAIL">Subscribe to our mailing list</label>
-                  <input type="email" value="" name="EMAIL" className="email" id="mce-EMAIL" placeholder="email address" required>
-                  </input>
-                  <div aria-hidden="true"><input name="b_59db66124013cd520fd3937ea_7312abcb99" tabIndex="-1" value=""></input></div> */}
+                <input type="email" value="" name="EMAIL" className="email" id="mce-EMAIL" placeholder="email address" required>
+              </input>
+              <div aria-hidden="true"><input name="b_59db66124013cd520fd3937ea_7312abcb99" tabIndex="-1" value=""></input></div> */}
                   <div className="clear"><input type="submit" value="Subscribe to our mailing list for new events info" name="subscribe" id="mc-embedded-subscribe" className="button"></input></div>
                 </div>
               </form>
@@ -108,24 +104,26 @@ class EventsShow extends React.Component {
               />
             </h3>
           </div>
-          <hr />
+
           {/* <div className="columns">
-          <div className="column is-offset-one-fifth"> */}
+      <div className="column is-offset-one-fifth"> */}
           <h3 className="title">Location</h3>
           <GoogleMap location={this.state.event.location} />
           {/* </div>
-          </div> */}
+      </div> */}
 
-          <h4 className="subtitle">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </h4>
+          {/* <h4 className="subtitle">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </h4> */}
 
-          <h1 className="title">Other events you may also like</h1>
+          <hr />
+
+          <h1 className="title">Other Events you may also like</h1>
 
           <div className="columns">
             <div className="column is-half is-offset-one-quarter">
               <Slider {...settings}>
                 {/* <Player>
-                  <source src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4" />
-                </Player> */}
+              <source src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4" />
+            </Player> */}
                 {/* <Link to={`/events/${event._id}`}> */}
                 {this.state.events.map(event =>
                   <div key={event._id} className="card">
