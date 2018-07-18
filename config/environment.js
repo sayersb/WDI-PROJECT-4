802828c70be25b14eb8f4fe545f25ad66c75152b
sayersb <bradleysayers1@gmail.com>
2018-07-18 09:36:07 +0100

    deploying now

--- config/environment.js
+++ config/environment.js
@@ -1,5 +1,5 @@
-const dbURI = 'mongodb://localhost:27017/events';
-const port = 4000;
+const dbURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/events';
+const port = process.env.PORT || 4000;
 const secret = 'aeorgheJEFJEKF';
 // const client = {
 //   sandbox: 'AU_mYjIkfpdm9UnQ7gQNzfjiKbAjqSMFPPpi7AchIM03oxmkPrfp5UktBkW43Igp1ItZUtD5jxSe8vTF',
